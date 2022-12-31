31796bf01fe48eb2c6ffade65d979700b786499c
iammilankhati <iammilankhati@gmail.com>
2022-12-31 15:06:09 +0545

    Mostly used js collected
    This file includes mostly used js things
    - map, filter,find, reduce
    - rest and spread operator
    - callback
    - promises
    - handling promises with async await and try catch
    - array.includes(item)

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+//Topics
+/* 
+1) map
+2) filter
+3) find
+4) reduce
+5) new Set()
+6) dynamic object keys
+7) array desctructing
+8) rest operator
+9) spread operator
+10) Optional chaining
+11) callback function
+12) Promises = (resolve,reject)
+13) handling promises using async await(try,catch) and .then.catch
+14) height/ width (window or any)
+15) TimeStamps
+16) array.at(index)
+17) element helper: querySelector, querySelectorAll
+18) for, forEach, 
+19) Object.keys(object).values().entries()
+20) string.includes('string || letter'), array.includes(item)
+*/
+
+/*==================================== Start==================================== */
 // 01: map method
 /* const people = [
 	{ name: 'John', age: 23, position: 'Employee' },
@@ -88,7 +113,7 @@ resultContainer.innerHTML = youngPeople
  */
 
 // 05 reduce -> reduces our array to a single value
-
+/* 
 const people = [
 	{ name: 'John', age: 23, position: 'Employee' },
 	{ name: 'Martin', age: 21, position: 'Employee' },
@@ -103,3 +128,177 @@ console.log(totalAge);
 
 const resultContainer = document.querySelector('.result');
 resultContainer.innerHTML = totalAge;
+ */
+
+// 06 Array desctructing
+/* const fruits = ['apple', 'banana', 'grapes', 'orange'];
+
+const [firstFruit] = fruits;
+console.log(firstFruit);
+
+const personInfo = {
+	name: 'John',
+	age: 23,
+};
+
+const { age: myAge } = personInfo;
+
+console.log(myAge); */
+
+//07 Rest operator
+
+// const friends = ['sunita', 'eliza', 'ram', 'sanam'];
+// const [sunita, ...restFriends] = friends;
+
+// console.log(restFriends);
+// console.log(sunita);
+
+// 08 Spread Operator
+/* const names = 'wikipedia';
+const info = {
+	name: 'ram',
+	id: 1,
+};
+
+const letters = [...names];
+console.log(letters);
+
+const infos = { ...info };
+console.log(infos); */
+
+// 09 Optional chaining
+
+// forEach(()=>{})
+// console.log(person?.location?.timezone?.offset || 'Hello World');
+
+// 10) callback function
+
+/* function makeUppercase(value) {
+	console.log(value.toUpperCase());
+}
+
+makeUppercase('Hello World');
+
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {
+	console.log('clicked');
+	setTimeout(() => {
+		btn.style.backgroundColor = 'green';
+		btn.style.color = 'red';
+	}, 3000);
+}); */
+//===========================================================
+// 11) Promises to eliminate callback hell
+
+// Promise lai handle garne deuta tarika cha
+// 1) async await => try, catch
+// 2) .then.catch
+//  promise can be inbuild of custom made
+//===========================================================
+// use to write async code sync way
+
+// async await or .then.catch()
+
+// consume/use promises
+// Pending , rejected, fulfilled
+/* 
+const promise = new Promise((resolve, reject) => {
+	// resolve('Hello world');
+	reject('there was a problem');
+});
+
+//cunsume in two ways: promise
+promise.then((result) => console.log(result)).catch((err) => console.log(err));
+const consume = async () => {
+	try {
+		const result = await promise;
+		console.log(result);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+consume();
+ */
+
+// 12) fetch api
+//handing promise using .then.catch
+/* fetch(url)
+	.then((result) =>
+		result
+			.json()
+			.then((result) => result)
+			.catch((err) => console.log(err))
+	)
+	.catch((err) => console.log(err));
+//handing promise using async await, try catch
+const getData = async () => {
+	try {
+		const result = await fetch(url);
+		const data = await result.json();
+	} catch (error) {
+		console.log(error);
+	}
+};
+ */
+
+// 13) width/height of any element or window
+// window.innerWidth;/*  */
+// windwo.innerHeight;
+// window.getBoundingClientRect();
+
+//14 timestamps
+/* console.log(new Date());
+console.log(Date.now());
+people = ['ram', 'shyam', { id: Date.now(), name: 'Hari' }];
+console.log(people);
+
+console.log(Date.now());
+console.log(new Date(1672474103376));
+const futureDate = new Date(Date.now + 60 * 1000);
+console.log(futureDate);
+
+console.log(new Date());
+console.log(Date.now());
+
+const firstDate = new Date();
+const secondDate = new Date(2021, 1, 6);
+
+console.log(secondDate);
+ */
+
+// 15 es2020
+// Array.at(-1); //at(index)
+
+// 16 element helper
+/* const element = document.querySelector('.classname or #id');
+const elementAll = document.querySelectorAll('.classname');
+
+const names = ['milan', 'asim']; */
+//17 for in loop
+/* for (const name in names) {
+	console.log(name);
+} */
+
+// 17 object.keys().values().entries()
+/* const info = {
+	name: 'milan',
+	age: 24,
+};
+console.log(Object.keys(info));
+console.log(Object.values(info));
+console.log(Object.entries(info)); */
+
+// 18 new Set() methods
+// const fruits = ['grapes', 'apple', 'orange', 'apple'];
+// const unique = new Set(fruits);
+// console.log(unique);
+
+// 19 string.includes array.includes
+// const firstName = 'john';
+// const result = firstName.includes('j', 0);
+// console.log(result);
+/* 
+const names = ['ram', 'shyam', 'karan', 'arjun'];
+const value = names.includes('shyam', 1);
+console.log(value); */
